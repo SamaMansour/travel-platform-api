c92910b401b7983af0556c2a7627541e7721d4bc
SamaMansour <sma302000@gmail.com>
2026-01-09 18:48:16 +0300

    call user module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/travel-platform', {
-      retryWrites: true,
-      w: 'majority',
-    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/travel-platform'),
+    UsersModule,
   ],
 })
 export class AppModule {}
