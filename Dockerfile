a403a1b6f80a4f684591cc1695036bcbc29623af
SamaMansour <sma302000@gmail.com>
2026-01-16 21:37:54 +0300

    add docker image dockerize my app

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:20-alpine
+
+WORKDIR /app
+
+COPY package*.json ./
+RUN npm install
+
+COPY . .
+
+RUN npx nest build
+
+EXPOSE 3000
+
+CMD ["node", "dist/main.js"]
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,16 +3,21 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { JwtStrategy } from './jwt-auth/jwt.strategy';
+import { RefreshStrategy } from './jwt-auth/refresh.strategy';
 
 @Module({
   imports: [
-    UsersModule, // ✅ IMPORTANT
+    UsersModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'dev_secret',
-      signOptions: { expiresIn: '15m' },
     }),
   ],
-  providers: [AuthService],
   controllers: [AuthController],
+  providers: [
+    AuthService,
+    JwtStrategy,       // ✅ REQUIRED
+    RefreshStrategy,   // ✅ REQUIRED (THIS FIXES jwt-refresh)
+  ],
 })
 export class AuthModule {}
